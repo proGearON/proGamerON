7747bdc84d53386bc533bab2b9ab7dcc29f85360
proGearON <145032836+proGearON@users.noreply.github.com>
2024-11-13 16:15:48 +0530

    Add files via upload

---
+++ excripted sector.js
@@ -0,0 +1,12 @@
+var password = "haudesh";
+     function passcheck() {
+          if(document.getElementById('haudesh').value != password) {
+               alert('Wrong password, Dialing proGearON Phone number......');
+               return false;
+          }
+     
+          if(document.getElementById('haudesh').value == password) {
+               alert('Hi Aryan, For Enter to privacy section Click on OK.');
+          }
+          
+          }
